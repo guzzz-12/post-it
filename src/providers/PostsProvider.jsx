c378fe4747d3c4c06f22e4dd26af7fde6f4bb1de
J3duardo <jesus_69eduardo@hotmail.com>
2019-12-19 14:58:03 -0400

    Context para administrar los posts

---
+++ src/providers/PostsProvider.jsx
@@ -0,0 +1,31 @@
+import React, {useEffect, useState, createContext} from 'react';
+import {firestore} from "../firebase";
+import {collectIdsAndDocs} from "../utils";
+
+export const PostContext = createContext();
+
+const PostsProvider = (props) => {
+  const [posts, setPosts] = useState([]);
+
+  let unsubscribeFromFirestore = null;
+
+  useEffect(() => {
+    // eslint-disable-next-line
+    unsubscribeFromFirestore = firestore.collection("posts").onSnapshot((snap) => {
+      const posts = snap.docs.map(collectIdsAndDocs);
+      setPosts(posts)
+    })
+
+    return () => {
+      unsubscribeFromFirestore();
+    }
+  }, [])
+
+  return (
+    <PostContext.Provider value={posts}>
+      {props.children}
+    </PostContext.Provider>
+  );
+}
+
+export default PostsProvider;
