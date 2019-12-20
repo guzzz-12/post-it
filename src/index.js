750d255768f2ec66b3105d6e9197f9b3bc970be9
J3duardo <jesus_69eduardo@hotmail.com>
2019-12-20 08:07:25 -0400

    Implementado React Router

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import './index.scss';
 import Application from './components/Application';
 import PostsProvider from './providers/PostsProvider';
 import UserProvider from './providers/UserProvider';
+import {BrowserRouter} from "react-router-dom";
 
 render(
-  <UserProvider>
-    <PostsProvider>
-      <Application />
-    </PostsProvider>
-  </UserProvider>,
+  <BrowserRouter>
+    <UserProvider>
+      <PostsProvider>
+        <Application />
+      </PostsProvider>
+    </UserProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
