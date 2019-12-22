8dc18815a761059ee53da75e6487b6c6a86cdcb6
J3duardo <jesus_69eduardo@hotmail.com>
2019-12-22 17:11:49 -0400

    Notificación de email no verificado
    Se muestra notificación al usuario en caso de que éste no haya verificado su email. El mismo componente ofrece la posibilidad de enviar otro email de verificación.

---
+++ src/components/EmailVerificationWarning.jsx
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+import {auth} from "../firebase";
+
+const EmailVerificationWarning = (props) => {
+  const [message, setMessage] = useState(null);
+
+  const sendVerification = () => {
+    if(auth.currentUser) {
+      auth.currentUser.sendEmailVerification({url: "http://localhost:3000/"})
+      .then(() => {
+        setMessage("Email verification sent, please check your inbox")
+        setTimeout(() => {
+          setMessage(null)
+        }, 3500)
+      })
+      .catch((error) => {
+        setMessage(error.message)
+      })
+    }
+  }
+
+  return (
+    <div
+      className={`${props.user && !props.user.emailVerified ?
+      "email-warning"
+      :
+      "email-warning email-warning--hidden"}`}
+      style={{marginBottom: `${props.user && !props.user.emailVerified ? "2rem" : "0"}`}}
+    >
+      {message &&
+        <h3>{message}</h3>
+      }
+      {props.user && !props.user.emailVerified && !message &&
+        <React.Fragment>
+          <p>You need to verify your email in order to create posts</p>
+          <p>Check your inbox or <span onClick={sendVerification}>Send email verification again</span> </p>
+        </React.Fragment>
+      }
+    </div>
+  );
+}
+
+export default EmailVerificationWarning;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,23 +2,30 @@ import React from "react";
 import {Link} from "react-router-dom";
 import WithUser from "./WithUser";
 import CurrentUser from "./CurrentUser";
+import EmailVerificationWarning from "./EmailVerificationWarning";
 
 const Nav = (props) => {
   return (
-    <nav className="main-nav">
-      <div className="main-nav__content">
-        <Link to="/"><h1>Think Piece</h1></Link>
-        {!props.user ?
-          <div className="main-nav__signin-signup">
-            <Link to="/signin-signup">Sing In or Sign Up</Link>
-          </div>
-          :
-          <div className="main-nav__current-user">
-            <CurrentUser user={props.user} />
-          </div>
-        }
-      </div>
-    </nav>
+    <React.Fragment>
+      <nav
+        className="main-nav"
+        style={{marginBottom: `${!props.user || (props.user && props.user.emailVerified) ? "2rem" : "0"}`}}
+      >
+        <div className="main-nav__content">
+          <Link to="/"><h1>Think Piece</h1></Link>
+          {!props.user ?
+            <div className="main-nav__signin-signup">
+              <Link to="/signin-signup">Sing In or Sign Up</Link>
+            </div>
+            :
+            <div className="main-nav__current-user">
+              <CurrentUser user={props.user} />
+            </div>
+          }
+        </div>
+      </nav>
+      <EmailVerificationWarning user={props.user} />
+    </React.Fragment>
   );
 }
 
