9727d1b5af01c7835c9e9dd4dca16c94fa0a99d3
J3duardo <jesus_69eduardo@hotmail.com>
2019-12-23 11:27:13 -0400

    Mostrar spiner mientras carga la página principal

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,10 +1,32 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState, useEffect} from 'react'
 import Post from './Post';
 import { PostContext } from '../providers/PostsProvider';
 import WithUser from './WithUser';
+import Spinner from './Spinner/Spinner';
 
 const Posts = (props) => {
   const posts = useContext(PostContext);
+  
+  const [isLoading, setIsLoading] = useState(true);
+  const [postsLoaded, setPostsLoaded] = useState(false);
+  const [userLoaded, setUserLoaded] = useState(false);
+
+  useEffect(() => {
+    setPostsLoaded(true)
+  }, [posts])
+  
+  useEffect(() => {
+    if(postsLoaded) {
+      setIsLoading(false)
+    }
+  }, [postsLoaded])
+
+  useEffect(() => {
+    if(props.user) {
+      setUserLoaded(true)
+    }
+  }, [props.user])
+
   const renderPosts = () => {
     return posts.map(post => {
       return <Post {...post} key={post.id} />
@@ -12,15 +34,18 @@ const Posts = (props) => {
   }
 
   return (
-    <section className="Posts">
-      {posts.length > 0 && <h2 className="Posts__title">Posts</h2>}
-      {renderPosts()}
-      {!props.user &&
-        <div className="Posts__message">
-          <h2>Login to star creating your posts!</h2>
-        </div>
-      }
-    </section>
+    <React.Fragment>
+      {isLoading && <Spinner />}
+      <section className="Posts">
+        {posts.length > 0 && <h2 className="Posts__title">Posts</h2>}
+        {renderPosts()}
+        {userLoaded && !props.user && !isLoading &&
+          <div className="Posts__message">
+            <h2>Login to star creating your posts!</h2>
+          </div>
+        }
+      </section>
+    </React.Fragment>
   )
 }
 
