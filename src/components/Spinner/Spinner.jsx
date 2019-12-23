1600703795db88f9b3c20d7b0cfb81b569c9e9bf
J3duardo <jesus_69eduardo@hotmail.com>
2019-12-23 11:26:03 -0400

    Spinner para indicar la carga de datos

---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./spinner.scss";
+
+const Spinner = (props) => {
+  return (
+    <div
+      className="spinner-container"
+      style={{
+        backgroundColor: `${props.transparent && "transparent"}`,
+      }}
+    >
+      <div
+        className="loader"
+        style={{
+          width: `${props.small && "2rem"}`,
+          height: `${props.small && "2rem"}`,
+          borderLeft: `${props.white && "0.4rem solid #fff"}`
+        }}
+      >
+        Loading...
+      </div>      
+    </div>
+  );
+}
+
+export default Spinner;
